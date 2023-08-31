3ab016393d87827474ec8d20eb4b19d67a353362
Callum <Callumr714@icloud.com>
2023-08-31 21:06:03 +0100

    Started Creating Distance Script

---
+++ galaxyDistanceApproximation.py
@@ -0,0 +1,11 @@
+# angular size and actual size (estimation) to distance :)
+def distanceIni(pixel_scale,bound_file, denoise=False):
+    pass
+
+def GetGalaxySizeFromArray(img, bound): # longer names sound more professional, right?
+    # use galaxy detection to draw boundrays right?
+    pass
+
+def GetGalaxyDistanceFromSize():
+    pass
+
