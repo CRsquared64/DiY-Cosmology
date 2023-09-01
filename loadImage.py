01680fe371be19e86e5dde55d737ea2e270427fb
callum <Callumr714@icloud.com>
2023-09-01 15:35:00 +0100

    Added Contrast

--- loadImage.py
+++ loadImage.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+from PIL import Image, ImageEnhance
 
 # going to include comments from now on, i have evolved
 def load_image(filepath):  # function to load image... wow
@@ -14,6 +14,15 @@ def image_conversions(img):  # takes the image, gets values, makes it gray and t
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     arr = np.array(gray)
     return arr, h, w
+
+def image_contrast(img, factor):
+    img = Image.fromarray(np.uint8(img))
+    enhancer = ImageEnhance.Contrast(img)
+    img = enhancer.enhance(factor)
+    img = np.array(img)
+    return img
+
+
 
 
 def clean(arr):  # denoiser, may change to make my own if i can be botherd (i cant)
