2cece19a8b112fa68fa2ab6185eafcbccaef7fe9
callum <Callumr714@icloud.com>
2023-09-01 20:10:34 +0100

    Better Denoise

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import loadImage
 import galaxyDetection
+import cv2
 
 denoise = True
 
 if __name__ == "__main__":
-    img = loadImage.load_image("images/m51.tif")
+    img = loadImage.load_image("images/outputc.tif")
     if denoise:
-        img = loadImage.clean(img)
-    img = loadImage.image_contrast(img, factor=5)
+        img = loadImage.clean(img, factor=25)
+        cv2.imshow("Denoised", img)
+    img = loadImage.image_contrast(img, factor=6)
     arr, h, w = loadImage.image_conversions(img)
 
-    galaxyDetection.fastCv2MaskPrediction(arr)
+    mask = galaxyDetection.fastCv2MaskPrediction(arr, thresh=75)
+    w = galaxyDetection.fastCv2StarlessPrediction(mask, 1, 7, 1)
+    print(f"{w} Pixel Diameter in mask")
